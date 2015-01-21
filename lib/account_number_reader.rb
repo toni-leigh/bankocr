717989f8e82d9a0c87ae2f1d2861ab6d19227457
Toni Leigh <toni.leigh@sage.com>
2015-01-21 15:56:22 +0000

    make account numbers correct type (Array)

--- lib/account_number_reader.rb
+++ lib/account_number_reader.rb
@@ -1,10 +1,12 @@
 class AccountNumberReader
 
-	attr_accessor :account_numbers_string, :account_numbers_source
+	attr_accessor :account_numbers_string, :account_numbers_source, :account_numbers
 
 	def initialize(account_numbers_source = 'data/use_case_1.txt')
 
 		@account_numbers_source = account_numbers_source
+
+		@account_numbers = []
 
 	end
 
