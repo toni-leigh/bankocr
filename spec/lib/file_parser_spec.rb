b21454fd221176d1129f90df9418cb2b8facaa85
Toni Leigh <toni.leigh@sage.com>
2015-01-21 11:15:13 +0000

    adds two expectations of the file parser class

--- spec/lib/file_parser_spec.rb
+++ spec/lib/file_parser_spec.rb
@@ -3,4 +3,12 @@ require "file_parser"
 
 describe FileParser do
 
+	it "should read the file from source into memory" do
+
+	end
+
+	it "should create an array of account number objects" do
+
+	end
+
 end
