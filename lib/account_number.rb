f94dc2a664ab2e3eaed3fc8a52c557f1327da941
Toni Leigh <toni.leigh@sage.com>
2015-01-22 09:35:21 +0000

    creates an array of digit objects in account number

--- lib/account_number.rb
+++ lib/account_number.rb
@@ -1,10 +1,16 @@
 class AccountNumber
 
-	attr_accessor :account_number_string
+	attr_accessor :account_number_string, :digits
 
 	def initialize(account_number_string = '')
 
 		@account_number_string = account_number_string
+
+		@digits = Array.new(9)
+
+		@digits.each_with_index do |digit,index|
+			@digits[index] = Digit.new
+		end
 
 	end
 
