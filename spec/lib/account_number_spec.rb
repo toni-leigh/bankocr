3130e4b4579b411722d72cb7d0b57404b449cdb4
Toni Leigh <toni.leigh@sage.com>
2015-01-22 08:59:08 +0000

    pass the string account number initlisation test crudely

--- spec/lib/account_number_spec.rb
+++ spec/lib/account_number_spec.rb
@@ -3,6 +3,16 @@ require "account_number"
 
 describe AccountNumber do
 
+	let(:account_number) { AccountNumber.new(' _  _  _  _  _  _  _  _  _ | || || || || || || || || ||_||_||_||_||_||_||_||_||_|                           ') }
+
+	it "should have a string that represents the full number of 108 chars" do
+
+		expect( account_number.account_number_string ).to be_kind_of (String)
+
+		expect( account_number.account_number_string.length).to be == 108
+
+	end
+
 	xit "should have an array of digit objects" do
 
 	end
