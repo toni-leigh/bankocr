0f4c29880887d276641eab52fcdeeda4af56a08c
Toni Leigh <toni.leigh@sage.com>
2015-01-22 11:14:26 +0000

    adds functionality to convert strings to integers including class variable to store the strings and their corresponding integers

--- lib/digit.rb
+++ lib/digit.rb
@@ -1,8 +1,29 @@
 class Digit
 	attr_accessor :number, :string
 
-	def initialize
+	@@string_representations = {
+		' _ | ||_|' => 0,
+		'     |  |' => 1,
+		' _   ||_ ' => 2,
+		' _  _| _|' => 3,
+		'   |_|  |' => 4,
+		' _ |_  _|' => 5,
+		' _ |_ |_|' => 6,
+		' _   |  |' => 7,
+		' _ |_||_|' => 8,
+		' _ |_| _|' => 9
+	}
+
+	def self.get_string_representations
+		@@string_representations
+	end
+
+	def initialize(string = '')
 		@number = 1
-		@string = '123456789'
+		@string = string
+	end
+
+	def convert_to_integer
+		@@string_representations[@string]
 	end
 end
