034c376987f77cc9cdf11ae6c9e3e77568fcefae
Toni Leigh <toni.leigh@sage.com>
2015-01-22 13:30:01 +0000

    adds method to extract digits from a full account number string, and uses this method when initialising Digits

--- lib/account_number.rb
+++ lib/account_number.rb
@@ -8,9 +8,27 @@ class AccountNumber
 
 		@digits = Array.new(9)
 
+		convert_account_number
+
+	end
+
+	def convert_account_number
+
 		@digits.each_with_index do |digit,index|
-			@digits[index] = Digit.new
+
+			@digits[index] = Digit.new(retrieve_digit_string(index))
+
 		end
+
+	end
+
+	def retrieve_digit_string(position)
+
+		[ 
+			@account_number_string[position * 3,3],
+			@account_number_string[position * 3 + 27,3],
+			@account_number_string[position * 3 + 54,3]
+		].join('')
 
 	end
 
