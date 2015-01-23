e89e7a26ea5d62abc413964398f78c817ff004dd
Toni Leigh <toni.leigh@sage.com>
2015-01-23 10:02:37 +0000

    edits data file and driver to reflect internal changes

--- bankocr_driver.rb
+++ bankocr_driver.rb
@@ -5,5 +5,5 @@ require "./lib/digit.rb"
 account_number_reader = AccountNumberReader.new()
 puts account_number_reader.account_numbers.length
 account_number_reader.account_numbers.each do |acc_num|
-	puts acc_num.humanised_output
+	puts acc_num.to_s
 end
