747b6ffbfdbc414b61e71bee0511b02441e729a6
Toni Leigh <toni.leigh@sage.com>
2015-01-26 13:41:46 +0000

    remove output logic from driver file as this is now in the to_s nmethod

--- bankocr_driver.rb
+++ bankocr_driver.rb
@@ -5,10 +5,5 @@ require "./lib/digit.rb"
 account_number_reader = AccountNumberReader.new()
 puts account_number_reader.account_numbers.length
 account_number_reader.account_numbers.each do |acc_num|
-	puts '=================='
 	puts acc_num.to_s
-	puts '------------------'
-	acc_num.alternate_numbers.each do |alt|
-		puts alt.to_s
-	end
 end
