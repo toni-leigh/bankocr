e13eafde5add43aea775e1eef92af6c22024fb45
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-12 13:58:53 +0000

    removed duplicated example digits from digit spec

--- spec/lib/digit_spec.rb
+++ spec/lib/digit_spec.rb
@@ -3,11 +3,6 @@ require "digit"
 
 describe Digit do
 
-	let(:digit) { 
-		Digit.new(' _ ' +
-						  '| |' +
-						  '|_|') 
-	}
 	let(:valid_digit) { 
 		Digit.new(' _ ' +
 						  '| |' +
@@ -25,19 +20,19 @@ describe Digit do
 	}
 
 	it "should have an integer value" do
-		expect( digit.number ).to be_kind_of (Integer)
+		expect( valid_digit.number ).to be_kind_of (Integer)
 	end
 
 	it "should be in the range of 0 and 9" do		
-		expect( digit.number ).to be_between(0,9)
+		expect( valid_digit.number ).to be_between(0,9)
 	end
 
 	it "should have a string representation" do
-		expect( digit.string ).to be_kind_of (String)
+		expect( valid_digit.string ).to be_kind_of (String)
 	end
 
 	it "should have a string length of 9" do
-		expect( digit.string.length ).to be == 9
+		expect( valid_digit.string.length ).to be == 9
 	end
 
 	it "should convert any strings for any digit into integers" do
@@ -53,16 +48,16 @@ describe Digit do
 	end
 
 	it "should output itself as a char, including a '?' if it is invalid" do
-		expect( digit.to_s ).to be_kind_of (String)
+		expect( valid_digit.to_s ).to be_kind_of (String)
 		expect( valid_digit.to_s ).to be == '0'
 		expect( illegible_digit.to_s ).to be =='?'
 	end
 
 	it "should return possible alternatives if it can be ambiguous" do
-		expect( digit.set_from_integer(0).get_alternates ).to be_kind_of (Array)
-		expect( digit.set_from_integer(5).get_alternates.length ).to be == 2
-		expect( digit.set_from_integer(9).get_alternates.length ).to be == 3
-		expect( digit.set_from_integer(4).get_alternates.length ).to be == 0
+		expect( subject.set_from_integer(0).get_alternates ).to be_kind_of (Array)
+		expect( subject.set_from_integer(5).get_alternates.length ).to be == 2
+		expect( subject.set_from_integer(9).get_alternates.length ).to be == 3
+		expect( subject.set_from_integer(4).get_alternates.length ).to be == 0
 	end
 
 	it "should know if it's salvagable or not, i.e. if there's just one char difference between itself and a valid string" do
