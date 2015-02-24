aa605daa22bc98fdc312d27bc50be33df73b9960
Toni-Leigh Sharpe <toni.leigh@sage.com>
2015-02-24 15:53:25 +0000

    removed erroneous .rb from file includes

--- bankocr_driver.rb
+++ bankocr_driver.rb
@@ -1,6 +1,6 @@
-require "./lib/account_number_reader.rb"
-require "./lib/account_number.rb"
-require "./lib/digit.rb"
+require "./lib/account_number_reader"
+require "./lib/account_number"
+require "./lib/digit"
 
 account_number_reader = AccountNumberReader.new()
 puts account_number_reader.account_numbers.length
